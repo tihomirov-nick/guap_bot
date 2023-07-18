91b108dbfce8f2bf93f40f5813f6d62da009e876
Nick <111696823+tihomirov-nick@users.noreply.github.com>
2023-07-18 11:52:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,29 @@ from bs4 import BeautifulSoup
 from collections import defaultdict
 
 from create import dp
-from handlers import client, admin
+from handlers import client
 from database import database
 
 
-def get_rasp(send_day):
-    url = 'https://guap.ru/rasp/?g=189'
+def format_schedule(text: str, day: str) -> str:
+    lines = text.split('\n')
+    formatted_text = f'Расписание на завтра:\n\n☀️ {day}\n'
+    for line in lines:
+        if 'пара' in line:
+            time = line.split(')')[0] + ')'
+            formatted_text += f'\n🕘 {time}\n'
+        else:
+            formatted_text += f'{line}\n'
+    return formatted_text
+
+
+def get_rasp(send_day, group):
+    url = f'https://guap.ru/rasp'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    option = soup.find('option', string=str(group))
+    value = option['value']
+    url = f'https://guap.ru/rasp/?g={value}'
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -23,8 +40,7 @@ def get_rasp(send_day):
     current_day = None
 
     found_h3 = False
-
-    for tag in soup.find_all(['h3', 'h4', 'div']):
+    for tag in soup.find_all(['h3', 'h4', 'span']):
         if tag.name == 'h3':
             found_h3 = True
             current_day = tag.text
@@ -36,7 +52,7 @@ def get_rasp(send_day):
     for day, lessons in schedule.items():
         if str(day) == str(send_day):
             for lesson in lessons:
-                text += lesson
+                text += '\n' + lesson
 
     return text
 
@@ -54,8 +70,11 @@ async def scheduler():
                 days_of_week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
                 tomorrow = datetime.now() + timedelta(days=1)
                 day_of_week = days_of_week[tomorrow.weekday()]
-                await dp.bot.send_message(user_id, get_rasp(day_of_week))
-                conn.commit()
+                group = c.execute('SELECT "group" FROM groups WHERE id = ?', (user_id,)).fetchone()[0]
+                print(group)
+                rasp = get_rasp(day_of_week, group)
+                formatted_schedule = format_schedule(rasp, day_of_week)
+                await dp.bot.send_message(user_id, formatted_schedule)
         await asyncio.sleep(60)
 
 
