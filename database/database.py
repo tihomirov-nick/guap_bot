9c703477dbd513d1f61f7496d6a50b9914c54ab1
Alexander <90530182+malashite@users.noreply.github.com>
2023-07-18 12:22:07 +0300

    Update database.py
    снеси ебало

--- database/database.py
+++ database/database.py
@@ -1,40 +1,62 @@
 import sqlite3 as sq
 
-def sql_start():
-    global base, cur
-    base = sq.connect('data.db')
-    cur = base.cursor()
-    cur.execute('''
-        CREATE TABLE IF NOT EXISTS groups (
-            id INTEGER PRIMARY KEY,
-            "group" TEXT
-        );
-    ''')
-    base.commit()
-    cur.execute('''
-        CREATE TABLE IF NOT EXISTS files (
-            id INTEGER PRIMARY KEY,
-            doc_id TEXT,
-            name TEXT
-        );
-    ''')
-    base.commit()
 
-# Files
-async def add_file(doc_id, name):
-    cur.execute('INSERT INTO files (doc_id, name) VALUES (?, ?)', (doc_id, name))
+def sql_start():
+	global base, cur
+	base = sq.connect('data.db')
+	cur = base.cursor()
+	cur.execute('''
+		CREATE TABLE IF NOT EXISTS groups (
+			id INTEGER PRIMARY KEY,
+			"group" TEXT
+			);
+		''')
+	base.commit()
+        
+	cur.execute('''
+		CREATE TABLE IF NOT EXISTS files (
+			id INTEGER PRIMARY KEY,
+			doc_id TEXT,
+            name TEXT,
+            description TEXT
+			);
+		''')
+	base.commit()
+
+
+#Файлы
+async def add_file(doc_id, name, description):
+    cur.execute('INSERT INTO files (doc_id, name, description) VALUES (?, ?, ?)', (doc_id, name, description)) 
     base.commit()
 
 async def delete_file(name):
-    cur.execute('DELETE FROM files WHERE name = ?', (name,))
+    cur.execute('DELETE FROM files WHERE name = ?', (name,))  
     base.commit()
-
+    
 async def file_exists(file_name: str) -> bool:
     cur.execute("SELECT COUNT(*) FROM files WHERE name=?", (file_name,))
     result = cur.fetchone()[0]
-    return result > 0
 
-# Groups
+    # Если количество найденных совпадений больше 0, то файл существует
+    if result > 0:
+        return True
+    else:
+        return False
+
+async def search_files_in_db(keywords):
+    keywords = keywords.lower()
+    results = cur.execute("SELECT name, description FROM files WHERE LOWER(name) LIKE ? OR LOWER(description) LIKE ?", ('%'+keywords+'%', '%'+keywords+'%')).fetchall()
+    print(results)
+    files = []
+    for result in results:
+        file = {
+            'name': result[0],
+            'description': result[1]
+        }
+        files.append(file)
+    return files
+
+#Группы
 async def get_group(id):
     return cur.execute('SELECT "group" FROM groups WHERE id=?', (id,)).fetchone()[0]
 
@@ -45,3 +67,4 @@ async def set_group_in_db(id, group):
 async def change_group_in_db(id, group):
     cur.execute('UPDATE groups SET "group" = ? WHERE id = ?', (group, id))
     base.commit()
+    
