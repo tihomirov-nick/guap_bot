94f117e4d57ca0d6351540bef896c041a5295f32
Nick <111696823+tihomirov-nick@users.noreply.github.com>
2023-07-20 00:43:58 +0300

    Update database.py

--- database/database.py
+++ database/database.py
@@ -40,7 +40,7 @@ async def get_group_by_id(user_id):
     return cur.execute('SELECT "group" FROM groups WHERE id = ?', (user_id,)).fetchone()[0]
 
 
-#Файлы
+# Files funcs
 async def add_file(doc_id, name, description):
     cur.execute('INSERT INTO files (doc_id, name, description) VALUES (?, ?, ?)', (doc_id, name, description)) 
     base.commit()
@@ -52,21 +52,19 @@ async def delete_file(name):
 async def file_exists(file_name: str) -> bool:
     cur.execute("SELECT COUNT(*) FROM files WHERE name=?", (file_name,))
     result = cur.fetchone()[0]
-
-    # Если количество найденных совпадений больше 0, то файл существует
-    if result > 0:
+    if result:
         return True
     else:
         return False
 
-#Группы
+
+# Groups funcs
 async def get_group(id):
     result = cur.execute('SELECT "group" FROM groups WHERE id=?', (id,)).fetchone()
     if result is not None:
         return result[0]
     else:
         return None
-
 
 async def set_group_in_db(id, group):
     cur.execute('INSERT INTO groups VALUES (?, ?)', (id, group))
